e8d4d2f6147bb3a6989c96dc1d17610983851bf5
Luchao Qi <lc.qi@outlook.com>
2021-09-09 18:41:00 -0400

    Luchao's auto-commitment using shell script

--- Scripts/pca.py
+++ Scripts/pca.py
@@ -1,24 +1,26 @@
 import numpy as np
 
 def pca(data):
+    # scale data based on mean of features
+    feature_mean = np.mean(data.T,axis=1)
+    data = data - feature_mean
 
-    covMatrix = np.cov(data,bias=True)
+    covMatrix = np.cov(data.T)
     eigenValues, eigenVectors = np.linalg.eig(covMatrix)
 
     idx = eigenValues.argsort()[::-1]   
     eigenValues = eigenValues[idx]
     eigenVectors = eigenVectors[:,idx]
 
+    projections = eigenVectors.T @ data.T
+    projections = projections.T
 
-    loadings = eigenVectors * np.sqrt(eigenValues)
-
-    print('Loadings of PC1')
-    print(loadings[:,0])
-    print('Projections on PC1')
-    print(data.T@loadings[:,0])
-    
+    print(eigenValues)
+    print(eigenVectors)
+    print(projections)
+    # projections[0] is the projection of data[0] on the new coordinates
 
 x = [-1,-1,0,2,0]
 y = [-2,0,0,1,1]
-data = np.array([x,y])
+data = np.array([[i,j] for i,j in zip(x,y)])
 pca(data)
